0a663d160d69976ad556519b677e473b64d91cd7
Maiki2002 <maiki200202@gmail.com>
2026-02-13 22:49:31 -0600

    The calendar and dependencies were included in the supabase

--- src/pages/Mobile/ProgramacionCxMobile.jsx
+++ src/pages/Mobile/ProgramacionCxMobile.jsx
@@ -1,11 +1,41 @@
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { useState } from "react";
+import { Calendar, LocaleConfig } from "react-native-calendars";
+
+LocaleConfig.locales['fr'] = {
+    monthNames: [
+        'Enero',
+        'Febrero',
+        'Marzo',
+        'Abril',
+        'Mayo',
+        'Junio',
+        'Julio',
+        'Agosto',
+        'Septiembre',
+        'Octubre',
+        'Noviembre',
+        'Diciembre'
+    ],
+    dayNames: [ 'Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado' ],
+    dayNamesShort: [ 'Dom', 'Lun', 'Mar', 'Mier', 'Jue', 'Vier', 'Sab']
+}
+
+LocaleConfig.defaultLocale = ['fr']
 
 export default function Cirugias(){
+    const [ selected, setSelected ] = useState('')
+
     return(
-        <View>
-            <Text>
-               Cirugias 
-            </Text>
-        </View>
+        <Calendar 
+        onDayPress={dia =>{
+            setSelected(dia.dateString)
+        }}
+        markedDates={{
+            [selected]: {selected:true
+            }        
+        }}
+        >
+        </Calendar>
     )
 }
